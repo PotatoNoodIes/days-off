fc5c3ab5361de193b510ce9ac19fc19332d1014b
Jose Henriquez <luis.henriquezca95@gmail.com>
2026-01-10 19:45:57 -0500

    Delete dist from tracking in git

--- apps/backend/dist/schedules/schedules.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { Schedule } from './entities/schedule.entity';
-export declare class SchedulesService {
-    private scheduleRepo;
-    constructor(scheduleRepo: Repository<Schedule>);
-    getForDateRange(start: Date, end: Date): Promise<Schedule[]>;
-    create(data: Partial<Schedule>): Promise<Schedule>;
-}
