5a027f72f767b03f5c14b17cc14487ead41c7064
Jose Henriquez <luis.henriquezca95@gmail.com>
2026-01-15 23:09:50 -0500

    Docker changes and working on different device

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import Constants from 'expo-constants';
 
-const BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:3000';
+const BASE_URL = Constants.expoConfig?.extra?.apiUrl || 'http://localhost:3000';
 
 export const api = axios.create({
   baseURL: BASE_URL,
